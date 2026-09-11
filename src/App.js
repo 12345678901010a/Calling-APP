9dd29015ae655f41a8aa7e4b087623f95da29946
12345678901010a <achow3163@hudsoncsd.org>
2026-09-11 18:34:30 -0400

    Add src/App.js - React main component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Discord Clone - Video Calling App</h1>
+      <p>Welcome to the Discord Clone with WebRTC and PeerJS</p>
+    </div>
+  );
+}
+
+export default App;
